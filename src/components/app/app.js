6c3102791f9f40e772f093f09e2d001d721524de
Dmitry <ddtihonov@yandex.ru>
2022-04-26 16:25:19 +0300

    1

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useReducer } from 'react';
 import styles from './app.module.css';
 import { Title } from '../../ui/title/title';
 import { Cart } from '../cart';
@@ -6,9 +6,24 @@ import { TotalPrice } from '../common/total-price';
 
 import { TotalPriceContext, DiscountContext } from '../../services/appContext';
 
+const discountInitialState = { discount: null }; 
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "set":
+      return { discount: action.payload };
+    case "reset":
+      return discountInitialState;
+    default:
+      throw new Error(`Wrong type of action: ${action.type}`);
+  }
+} 
+
 function App() {
   const [totalPrice, setTotalPrice] = useState(0);
   const [discount, setDiscount] = useState(null);
+
+  const [discountState, discountDispatcher] = useReducer(reducer, discountInitialState, undefined);
 
   return (
     <div className={styles.app}>
