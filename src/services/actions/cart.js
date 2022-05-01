a8a792719cbf023a823d40978849b8a5e38df68b
Dmitry <ddtihonov@yandex.ru>
2022-05-01 16:13:10 +0300

    6

--- src/services/actions/cart.js
+++ src/services/actions/cart.js
@@ -1,4 +1,4 @@
-import {getItemsRequest} from '../fakeApi';
+import {getItemsRequest, getRecommendedItemsRequest, applyPromoCodeRequest} from '../fakeApi';
 
 export const INCREASE_ITEM = 'INCREASE_ITEM'; //увеличение количества товаров,
 export const DECREASE_ITEM = 'DECREASE_ITEM';//уменьшение количества товаров,
@@ -13,6 +13,10 @@ export const GET_ITEMS_FAILED = 'GET_ITEMS_FAILED'//используется в 
 export const GET_RECOMMENDED_ITEMS_REQUEST = 'GET_RECOMMENDED_ITEMS_REQUEST'//отображается, если запрос отправлен.
 export const GET_RECOMMENDED_ITEMS_SUCCESS  = 'GET_RECOMMENDED_ITEMS_SUCCESS'//показывается в случае успеха, когда запрос выполнен и товары получены.
 export const GET_RECOMMENDED_ITEMS_FAILED = 'GET_RECOMMENDED_ITEMS_FAILED'//используется в случае ошибки при выполнении запроса.
+//применение промокодов
+export const APPLY_PROMO_FAILED = 'APPLY_PROMO_FAILED'//ошибка активации промокода,
+export const APPLY_PROMO_REQUEST = 'APPLY_PROMO_REQUEST'//произошёл запрос,
+export const APPLY_PROMO_SUCCESS = 'APPLY_PROMO_SUCCESS'//запрос выполнился успешно.
 
 export function getItems (){
     return (dispatch) => {
@@ -32,5 +36,47 @@ export function getItems (){
     });
     }
 }); 
-}
+};
+};
+
+export function getRecommendedItems () {
+    return (dispatch) => {
+        dispatch({
+            type: GET_RECOMMENDED_ITEMS_REQUEST
+        });
+
+        getRecommendedItemsRequest().then(res => {
+    if (res && res.success) {
+        dispatch({
+        type: GET_RECOMMENDED_ITEMS_SUCCESS,
+        items: res.data
+    });
+    } else {
+        dispatch({
+            type: GET_RECOMMENDED_ITEMS_FAILED
+    });
+    }
+}); 
+};
+};
+
+export function applyPromo (code) {
+    return (dispatch) => {
+        dispatch({
+            type: APPLY_PROMO_REQUEST
+        });
+
+        applyPromoCodeRequest(code).then(res => {
+    if (res && res.success) {
+        dispatch({
+        type: APPLY_PROMO_SUCCESS,
+        value: { ...res, code }
+    });
+    } else {
+        dispatch({
+            type: APPLY_PROMO_FAILED
+    });
+    }
+}); 
+};
 };
